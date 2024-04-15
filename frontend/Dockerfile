3145c4cff4661932d36f12afb4b613b7d02ecda5
Nikita <ldc.endoscopy@gmail.com>
2024-04-15 21:53:03 +0400

    Bare backend

--- frontend/Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:lts-alpine as build
-WORKDIR /app
-COPY package*.json ./
-RUN npm install
-COPY . ./
-RUN npm run build
-RUN cp -r build result_build
--- frontend/src/app/types/hooks.ts
+++
@@ -1,9 +0,0 @@
-import {
-  TypedUseSelectorHook,
-  useDispatch as useReduxDispatch,
-  useSelector as useReduxSelector,
-} from "react-redux";
-import { AppDispatch, RootState } from "../..";
-
-export const useDispatch = () => useReduxDispatch<AppDispatch>();
-export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
