9e2665e244dcca6fe4ede2e21c3e2f6b76a39943
nikita <ldc.endoscopy@gmail.com>
2024-04-26 11:48:13 +0300

    001

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,18 +89,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 if DEBUG:
     DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': BASE_DIR / 'db.sqlite3',
-    #     }
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'postgres',
-            'USER': 'postgres',
-            'PASSWORD': 'postgres',
-            'HOST': 'localhost', 
-            'PORT': '5432',
-        }    
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.postgresql',
+        #     'NAME': 'postgres',
+        #     'USER': 'postgres',
+        #     'PASSWORD': 'postgres',
+        #     'HOST': 'localhost', 
+        #     'PORT': '5432',
+        # }    
     }
     CELERY_BROKER_URL = 'redis://localhost:6379/0'
 else:
