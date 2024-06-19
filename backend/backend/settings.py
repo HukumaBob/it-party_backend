38b0c8b2800026bf372bdad462ae765eb0274cf4
nikita <ldc.endoscopy@gmail.com>
2024-06-19 20:47:41 +0300

    Pagination OFF in setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -184,7 +184,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    # 'PAGE_SIZE': 100
 }
 
 SIMPLE_JWT = {
