414cb55df44368174ba1e3e001059b6aa1ad5539
nikita <ldc.endoscopy@gmail.com>
2024-08-03 20:54:23 +0400

    Remove the +static() from urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -47,8 +47,8 @@ urlpatterns = [
         ),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-        )
+# if settings.DEBUG:
+#     urlpatterns += static(
+#         settings.MEDIA_URL,
+#         document_root=settings.MEDIA_ROOT
+#         )
