10a3f118ac890c5d057c84fa6e37da7e6aed911b
nikita <ldc.endoscopy@gmail.com>
2024-08-05 11:35:26 +0400

    const BASE_URL = ""

--- frontend/src/app/api/constants.ts
+++ frontend/src/app/api/constants.ts
@@ -25,7 +25,8 @@ type TEndpoints = {
 }
 
 // const BASE_URL = "http://localhost:8000"
-const BASE_URL = "https://itparty.ddns.net"
+const BASE_URL = ""
+// const BASE_URL = "https://itparty.ddns.net"
 // const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8000"
 
 const endpoints: TEndpoints = {
